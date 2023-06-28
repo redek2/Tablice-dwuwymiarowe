b5ee3d4671925274b5a947e93f23abcb967f9639
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-28 18:24:42 +0200

    Add files via upload

---
+++ Zadanie3.2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę wierszy i kolumn:");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        int[,] tablica = new int[n, n];
+
+        int liczba, k, l, m, a;
+        liczba = 1;
+        k = 0;
+        l = 0;
+        m = n;
+        a = n;
+
+        while (k < n && l < n)
+        {
+            for (int i = k; i < m; i++)
+            {
+                tablica[l, i] = liczba;
+                liczba++;
+            }
+
+            l++;
+            for (int i = l; i < m; i++)
+            {
+                tablica[i, n - 1] = liczba;
+                liczba++;
+            }
+            n--;
+            if (l < m)
+            {
+                for (int i = n - 1; i >= k; i--)
+                {
+                    tablica[m - 1, i] = liczba;
+                    liczba++;
+                }
+                m--;
+            }
+            if (k < n)
+            {
+                for (int i = m - 1; i >= l; i--)
+                {
+                    tablica[i, k] = liczba;
+                    liczba++;
+                }
+                k++;
+            }
+        }
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < a; j++)
+            {
+                Console.Write(tablica[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
